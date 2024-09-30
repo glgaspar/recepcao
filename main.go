18f36d22378656495f721d0ec7586cb984d53d3d
Gustavo Gaspar <glgaspar@live.com>
2024-09-29 21:49:17 -0300

    more data to render

--- main.go
+++ main.go
@@ -12,40 +12,40 @@ import (
 
 type Templates struct {
 	templates *template.Template
-} 
+}
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 type App struct {
-	Img string
+	Img  string
 	Name string
 	Path string
 }
 
-type AppList struct {
-	Apps []App
+type Config struct {
+	Apps   []App `json:"Apps"`
+	Config []App `json:"Config"`
 }
 
-func (a *AppList) Generate() error {
+func (c *Config) Generate() error {
 	file, err := os.Open("routes.json")
 	if err != nil {
 		return err
 	}
-	
+
 	bytesBuffer, err := io.ReadAll(file)
 	if err != nil {
 		return err
 	}
-	
-	if err = json.Unmarshal(bytesBuffer, &a.Apps); err!=nil{
+
+	if err = json.Unmarshal(bytesBuffer, &c); err != nil {
 		return err
 	}
 
 	return nil
 }
-
 
 func newTemplate() *Templates {
 	return &Templates{
@@ -53,12 +53,11 @@ func newTemplate() *Templates {
 	}
 }
 
-
 func main() {
-	e:= echo.New()
+	e := echo.New()
 	e.Use(middleware.Logger())
 
-	var data = AppList{}
+	var data = Config{}
 	data.Generate()
 
 	e.Static("/dist", "dist")
